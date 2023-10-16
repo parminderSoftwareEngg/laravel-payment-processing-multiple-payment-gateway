e4beb79094b76e7948f09a39c3ca11dac57099de
parminder <parminder.webdeveloper@gmail.com>
2023-10-16 10:42:20 +0530

    Paypal payment gateway integrated

---
+++ routes/web.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::post('/payments/pay', [App\Http\Controllers\PaymentController::class, 'pay'])->name('pay');
+Route::get('/payments/approval', [App\Http\Controllers\PaymentController::class, 'approval'])->name('approval');
+Route::get('/payments/cancelled', [App\Http\Controllers\PaymentController::class, 'cancelled'])->name('cancelled');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
